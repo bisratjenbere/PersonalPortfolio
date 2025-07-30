79119e6440decb45d9c4de7a909b6a63d3401abb
Bisrat <jenberehenok@gmail.com>
2025-07-30 22:34:41 +0300

    added

---
+++ src/portfolio.js
@@ -0,0 +1,414 @@
+const settings = {
+  isSplash: false,
+};
+
+const seo = {
+  title: "Bisrat's Portfolio",
+  description:
+    "I’m passionate about creating complete software solutions that solve real problems and scale to make a lasting difference.",
+  og: {
+    title: "Bisrat Jenbere Portfolio",
+    type: "website",
+    url: "http://ashutoshhathidara.com/",
+  },
+};
+
+//Home Page
+const greeting = {
+  title: "Bisrat Jenbere",
+  logo_name: "BisratJenbere",
+  nickname: "",
+  subTitle:
+    "A passionate problem-solver who thrives on building end-to-end products, learning quickly, and developing scalable, impactful solutions for real-world challenges.",
+  resumeLink:
+    "https://drive.google.com/file/d/1bXRknv_h-XI_3CQ3SGPteGODtvEb7YvI/view?usp=sharing",
+  portfolio_repository: "https://github.com/ashutosh1919/masterPortfolio",
+  githubProfile: "https://github.com/bisratjenbere",
+};
+
+const socialMediaLinks = [
+  {
+    name: "Github",
+    link: "https://github.com/bisratjenbere",
+    fontAwesomeIcon: "fa-github",
+    backgroundColor: "#181717",
+  },
+  {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/bisrat-jenbere-34a6a928a/",
+    fontAwesomeIcon: "fa-linkedin-in",
+    backgroundColor: "#0077B5",
+  },
+
+  {
+    name: "Gmail",
+    link: "mailto:jenberehenok@gmail.com",
+    fontAwesomeIcon: "fa-google",
+    backgroundColor: "#D14836",
+  },
+  {
+    name: "X-Twitter",
+    link: "https://x.com/bisru04",
+    fontAwesomeIcon: "fa-x-twitter",
+    backgroundColor: "#000000",
+  },
+];
+
+const skills = {
+  data: [
+    {
+      title: "Full Stack Development",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Building responsive website front end using React",
+        "⚡ Developing mobile applications using React Native",
+        "⚡ Creating application backend in Node, Express & Laravel",
+        "⚡ Working with databases like MongoDB and MySQL",
+        "⚡ Integrating RESTful APIs and third-party services (e.g Stripe)",
+        "⚡ Exploring AI/ML integration in real-world applications",
+        "⚡ Creating architecture diagrams for team communication",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "Sass",
+          fontAwesomeClassname: "simple-icons:sass",
+          style: {
+            color: "#CC6699",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#000000",
+            color: "#F7DF1E",
+          },
+        },
+        {
+          skillName: "TypeScript",
+          fontAwesomeClassname: "simple-icons:typescript",
+          style: {
+            backgroundColor: "#000000",
+            color: "#3178C6",
+          },
+        },
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "NodeJS",
+          fontAwesomeClassname: "devicon-plain:nodejs-wordmark",
+          style: {
+            color: "#339933",
+          },
+        },
+        {
+          skillName: "NPM",
+          fontAwesomeClassname: "simple-icons:npm",
+          style: {
+            color: "#CB3837",
+          },
+        },
+        {
+          skillName: "Yarn",
+          fontAwesomeClassname: "simple-icons:yarn",
+          style: {
+            color: "#2C8EBB",
+          },
+        },
+        {
+          skillName: "Laravel",
+          fontAwesomeClassname: "simple-icons:laravel",
+          style: {
+            backgroundColor: "#000000",
+            color: "#FF2D20",
+          },
+        },
+        {
+          skillName: "Python",
+          fontAwesomeClassname: "simple-icons:python",
+          style: {
+            backgroundColor: "#000000",
+            color: "#3776AB",
+          },
+        },
+
+        {
+          skillName: "PHP",
+          fontAwesomeClassname: "simple-icons:php",
+          style: {
+            backgroundColor: "#000000",
+            color: "#777BB4",
+          },
+        },
+      ],
+    },
+
+    {
+      title: "UI/UX Design",
+      fileName: "DesignImg",
+      skills: [
+        "⚡ Designing highly attractive user interface for mobile and web applications",
+        "⚡ Creating the flow of application functionalities to optimize user experience",
+      ],
+      softwareSkills: [
+        {
+          skillName: "Figma",
+          fontAwesomeClassname: "simple-icons:figma",
+          style: {
+            color: "#F24E1E",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+// Education Page
+const competitiveSites = {
+  competitiveSites: [
+    {
+      siteName: "LeetCode",
+      iconifyClassname: "simple-icons:leetcode",
+      style: {
+        color: "#F79F1B",
+      },
+      profileLink: "https://leetcode.com/u/bisratjenbere/",
+    },
+    {
+      siteName: "Red Hat",
+      iconifyClassname: "simple-icons:redhat",
+      style: {
+        color: "#EE0000",
+      },
+      profileLink: "https://www.redhat.com/",
+    },
+    {
+      siteName: "Cisco",
+      iconifyClassname: "simple-icons:cisco",
+      style: {
+        color: "#1BA0D7",
+      },
+      profileLink: "https://www.cisco.com/",
+    },
+
+    {
+      siteName: "Huawei",
+      iconifyClassname: "simple-icons:huawei",
+      style: {
+        color: "#FF0000",
+      },
+      profileLink: "https://www.huawei.com/",
+    },
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "Wolkite Univeristy Department Of Software Engineering ",
+      subtitle: "B.Tech. in Software Engineering",
+      logo_path: "wku.png",
+      alt_name: "WKU university",
+      duration: "2019 - 2024",
+      descriptions: [
+        "⚡ I have studied basic software engineering subjects like DS, Algorithms, DBMS, OS, CA, AI etc.",
+        "⚡ Apart from this, I have done courses on Ai and Full Stack Development.",
+        "⚡I graduated with a GPA of 3.71 and scored 83 on the national exit exam.",
+      ],
+      website_link: "https://www.wku.edu.et/en/index.php/en",
+    },
+  ],
+};
+const certifications = {
+  certifications: [
+    {
+      title: "HCIA-Datacome",
+      subtitle: "- huwaei",
+      logo_path: "huwaeilogo.png",
+      certificate_link:
+        "https://drive.google.com/file/d/15g3qJYMurxTwFUC3dYyDBijJjZgLgubD/view?usp=drive_link",
+      alt_name: "huwaei",
+      color_code: "#8C151599",
+    },
+    {
+      title: "Application development java EE ",
+      subtitle: "- redhat",
+      logo_path: "REDHATLOGO.svg",
+      certificate_link:
+        "https://drive.google.com/file/d/1ybeNlVDZbkTd0_YfmkCF3BBFhywEpSYt/view?usp=sharing",
+      alt_name: "redhat",
+      color_code: "#00000099",
+    },
+    {
+      title: "NDG Linux Essentials",
+      subtitle: "linux",
+      logo_path: "ciscologo.png",
+      certificate_link:
+        "https://drive.google.com/file/d/1shjeCksoIkmeTtMJBYY9PugTy_QfjHAY/view?usp=sharing",
+      alt_name: "linux",
+      color_code: "#0C9D5899",
+    },
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "Internship and project",
+  description:
+    "I have hands-on experience through internships and projects in ERP customization and software testing, where I contributed to improving workflows and ensuring product quality. In addition, I have developed practical skills in web development, working with modern technologies to build and optimize responsive applications. Beyond internships, I actively enhance my expertise by exploring real-world projects, contributing to open-source communities, and continuously improving my full‑stack development skills.",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Internships",
+      experiences: [
+        {
+          title: "ERP Customization Intern",
+          company: "WKU App Development",
+          company_url: "https://wku.edu.et/",
+          logo_path: "wku.png",
+          duration: "Jan 2022 - sept 2022",
+          location: "Wolkite, Ethiopia",
+          description:
+            "Contributed to quality assurance processes by testing multiple university systems including the Cafeteria Management System, Gate Management System, and Library Management System. Designed and executed functional and regression test cases to verify features, validate business logic, and ensure proper integration across modules. Documented and reported bugs, collaborated with developers to resolve issues, and re-tested fixes to guarantee stability. Played a key role in performance optimization by identifying bottlenecks, verifying security workflows, and ensuring the systems were reliable and user-friendly for day-to-day operations.",
+          color: "#0A66C2",
+        },
+
+        {
+          title: "Software Testing Intern",
+          company: "WKU App Development",
+          company_url: "https://wku.edu.et/",
+          logo_path: "wku.png",
+          duration: "jun 2023 - sept 2023",
+          location: "Wolkite, Ethiopia",
+          description:
+            "Contributed to quality assurance processes by testing multiple university systems including the Cafeteria Management System, Gate Management System, and Library Management System. Designed and executed functional and regression test cases to verify features, validate business logic, and ensure proper integration across modules. Documented and reported bugs, collaborated with developers to resolve issues, and re-tested fixes to guarantee stability. Played a key role in performance optimization by identifying bottlenecks, verifying security workflows, and ensuring the systems were reliable and user-friendly for day-to-day operations.",
+          color: "#0A66C2",
+        },
+      ],
+    },
+    {
+      title: "Volunteerships",
+      experiences: [
+        {
+          title: "Developer Program Member ",
+          company: "Wku",
+          company_url: "",
+          logo_path: "wku.png",
+          duration: "sept 2021 - july 2023",
+          location: "WKU UNIVERSITY",
+          description:
+            "I worked on developing and improving a web-based evaluation system for instructors. This included designing and implementing features for grading, feedback management, and performance tracking. I focused on building a user-friendly interface and ensuring the system was scalable, secure, and optimized for smooth performance. My work also involved writing clean, maintainable code, fixing bugs, and collaborating on feature enhancements to improve the overall evaluation process.",
+          color: "#181717",
+        },
+      ],
+    },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects makes use of vast variety of latest technology tools. My best experience is to create Data Science projects and deploy them to web applications using cloud infrastructure.",
+  avatar_image_path: "projects_image.svg",
+};
+
+const publicationsHeader = {
+  title: "Publications",
+  description: "Some of my published Articles, Blogs and Research.",
+  avatar_image_path: "projects_image.svg",
+};
+
+const publications = {
+  data: [
+    {
+      id: "neuro-symbolic-sudoku-solver",
+      name: "Neuro-Symbolic Sudoku Solver",
+      createdAt: "2023-07-02T00:00:00Z",
+      description: "Paper published in KDD KiML 2023",
+      url: "https://arxiv.org/abs/2307.00653",
+    },
+    {
+      id: "mdp-diffusion",
+      name: "MDP-Diffusion",
+      createdAt: "2023-09-19T00:00:00Z",
+      description: "Blog published in Paperspace",
+      url: "https://blog.paperspace.com/mdp-diffusion/",
+    },
+    {
+      id: "consistency-models",
+      name: "Consistency Models",
+      createdAt: "2023-10-12T00:00:00Z",
+      description: "Blog published in Paperspace",
+      url: "https://blog.paperspace.com/consistency-models/",
+    },
+  ],
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "animated_ashutosh.png",
+    description:
+      "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.",
+  },
+  blogSection: {
+    title: "Blogs",
+    subtitle:
+      "I like to document some of my experiences in professional career journey as well as some technical knowledge sharing.",
+    link: "https://blogs.ashutoshhathidara.com/",
+    avatar_image_path: "blogs_image.svg",
+  },
+  addressSection: {
+    title: "Address",
+    subtitle: "Saratoga Ave, San Jose, CA, USA 95129",
+    locality: "San Jose",
+    country: "USA",
+    region: "California",
+    postalCode: "95129",
+    streetAddress: "Saratoga Avenue",
+    avatar_image_path: "address_image.svg",
+    location_map_link: "https://maps.app.goo.gl/NvYZqa34Wye4tpS17",
+  },
+  phoneSection: {
+    title: "",
+    subtitle: "",
+  },
+};
+
+export {
+  settings,
+  seo,
+  greeting,
+  socialMediaLinks,
+  skills,
+  competitiveSites,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  publicationsHeader,
+  publications,
+  contactPageData,
+};
